f9533dfd39ddfe3859065cd43068678224e667a6
samue <samuelmagoncalves@gmail.com>
2023-02-19 09:07:04 -0300

    Created Weather class, for now it use location and the api_key as a parameter to create a Weather object, and uses methods like get_temperature and get_humidity to output information.

---
+++ WeatherAppProject/weathercall.py
@@ -0,0 +1,28 @@
+"""
+This module is responsible for making the API call to OpenWeatherMap.org
+Probably needs more testing, but it works for now.
+use Weather(location, api_key) to create a new weather object
+use get_temperature() to get the temperature in Celsius
+use get_humidity() to get the humidity in %
+use get_wind_speed() to get the wind speed in m/s
+"""
+import requests
+class Weather:
+    def __init__(self, location: str, api_key: str):
+        self.location = location
+        self.api_key = api_key
+    def get_weather(self):
+        url = f'https://api.openweathermap.org/data/2.5/weather?q={self.location}&appid={self.api_key}'
+        response = requests.get(url)
+        return response.json()
+
+    def get_temperature(self):
+        weather = self.get_weather()
+        temp = weather['main']['temp']
+        temp = temp - 273.15
+        return temp
+
+    def get_humidity(self):
+        weather = self.get_weather()
+        humidity = weather['main']['humidity']
+        return humidity
