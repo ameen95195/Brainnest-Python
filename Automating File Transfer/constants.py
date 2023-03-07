502da993c6201e56437fe7a82ebeb08299071728
amin alahmed <46929687+ameen95195@users.noreply.github.com>
2023-03-07 16:42:43 +0300

    setup trans project

---
+++ Automating File Transfer/constants.py
@@ -0,0 +1,14 @@
+# constants variables
+
+class Consts:
+    MSG_SUBJECT = "Subject"
+    MSG_FROM = "From"
+    MSG_TO = "To"
+    MSG_CONTENT = "message-content"
+    HOST_SERVER = "host-server"
+    PORT = "port"
+    SENDER_EMAIL = "sender-email"
+    SENDER_PASSWORD = "sender-password"
+    ATTACH_LOCATION = "attachment-location"
+    RECIPIENTS_EMAILS = "recipients-emails"
+    DAILY_SCHEDULE_TIME = "daily-schedule-time"
